8581442a0e565f1adab77e2be7c4434e152196c4
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-04-13 09:59:49 +0000

    - merge all SQL definitions in one file

--- trunk/web/tools/smonitor/tables.sql
+++ trunk/web/tools/smonitor/tables.sql
@@ -1,22 +1,27 @@
 -- 
--- Structura de tabel pentru tabelul `monitored_stats`
+-- Table for `monitored_stats`
 -- 
 
+DROP TABLE IF EXISTS `monitored_stats`;
 CREATE TABLE `monitored_stats` (
   `name` varchar(64) NOT NULL,
   `extra` varchar(64) NOT NULL,
-#  PRIMARY KEY  (`name`)
-) ENGINE=MyISAM;
+  `box_id` mediumint(8) unsigned NOT NULL default '0'
+  PRIMARY KEY  (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 -- 
--- Structura de tabel pentru tabelul `monitoring_stats`
+-- Table for `monitoring_stats`
 -- 
 
+DROP TABLE IF EXISTS `monitoring_stats`;
 CREATE TABLE `monitoring_stats` (
   `name` varchar(64) NOT NULL,
   `time` int(11) NOT NULL,
   `value` varchar(64) NOT NULL default '0',
-#  PRIMARY KEY  (`name`,`time`)
-) ENGINE=MyISAM;
+  `box_id` mediumint(8) unsigned NOT NULL default '0'
+  PRIMARY KEY  (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
