a0ca484ce89e7088c7449d5a137f351dacd4b0aa
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-04-13 12:37:59 +0000

    Bogus fixed

--- trunk/cron_job/generate-cdrs.sh
+++ trunk/cron_job/generate-cdrs.sh
@@ -4,7 +4,7 @@
 HOSTNAME="localhost"
 USER="opensips"
 PASS="opensips_pass"
-DATABASE="opensips_1_4"
+DATABASE="opensips"
 
-mysql -h $HOSTNAME -u $USER -p$PASS -e "call opensips_cdrs_1_4(); " $DATABASE
+mysql -h $HOSTNAME -u $USER -p$PASS -e "call opensips_cdrs_1_5(); " $DATABASE
 
