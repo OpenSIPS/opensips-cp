db04a73276d627227c7b6faf48c865139017c575
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-04-22 15:08:49 +0000

    Fixed bogus

--- branches/2.0/web/tools/smonitor/tables.sql
+++ branches/2.0/web/tools/smonitor/tables.sql
@@ -4,10 +4,9 @@
 
 DROP TABLE IF EXISTS `monitored_stats`;
 CREATE TABLE `monitored_stats` (
-  `name` varchar(64) NOT NULL,
+  `name` varchar(64) PRIMARY KEY NOT NULL,
   `extra` varchar(64) NOT NULL,
   `box_id` mediumint(8) unsigned NOT NULL default '0'
-  PRIMARY KEY  (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -18,7 +17,7 @@ CREATE TABLE `monitored_stats` (
 
 DROP TABLE IF EXISTS `monitoring_stats`;
 CREATE TABLE `monitoring_stats` (
-  `name` varchar(64) NOT NULL,
+  `name` varchar(64) PRIMARY KEY NOT NULL,
   `time` int(11) NOT NULL,
   `value` varchar(64) NOT NULL default '0',
   `box_id` mediumint(8) unsigned NOT NULL default '0'
