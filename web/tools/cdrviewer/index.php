36bc6cf9ab88d93b200d25f04eec6e25a0f346e2
iulia_bublea <iulia@opensips.org>
2009-06-03 13:38:42 +0000

    - cdrviewer files modified for postgres compatibility.

--- web/tools/cdrviewer/index.php
+++ web/tools/cdrviewer/index.php
@@ -1,10 +1,8 @@
 <? 
-
 require("lib/functions.inc.php");
 session_start();
 get_priv();
 $_SESSION['detailed_callid']=array();
 $_SESSION['grouped_results']=true;
 header("Location: cdrviewer.php");
-
 ?> 
