0bbf64eabf328645431e29dc841ac8e34ecc62d5
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-06-03 13:30:11 +0000

    Cron job files modified so that opensips-cp would work with mysql and postgresql db.

--- trunk/cron_job/get_opensips_stats.php
+++ trunk/cron_job/get_opensips_stats.php
@@ -2,10 +2,12 @@
 
 $opensips_path="/var/www/opensips-cp/";
 require($opensips_path."config/tools/smonitor/db.inc.php");
+require($opensips_path."config/db.inc.php");
 require($opensips_path."config/tools/smonitor/local.inc.php");
 require($opensips_path."web/tools/smonitor/lib/functions.inc.php");
 require($opensips_path."web/common/mi_comm.php");
 require($opensips_path."config/boxes.global.inc.php");
+require($opensips_path."web/tools/smonitor/lib/db_connect.php");
 
 
 $box_id=0;
@@ -28,21 +30,26 @@ foreach ($boxes as $ar){
 			$comm_type=params($ar['mi']['conn']); 		
  			$stats=get_all_vars();
 	
-			$result=mysql_query("SELECT * FROM ".$config->table_monitored." WHERE extra='' AND box_id=".$box_id." ORDER BY name ASC") or die(mysql_error());
- 			echo "SELECT * FROM ".$config->table_monitored." WHERE extra='' AND box_id=".$box_id." ORDER BY name ASC\n";		
- 			while($row=mysql_fetch_array($result))
+			$sql = "SELECT * FROM ".$config->table_monitored." WHERE extra='' AND box_id=".$box_id." ORDER BY name ASC";
+			$resultset = $link->queryAll($sql);
+			if(PEAR::isError($resultset))
+		                die('Failed to issue query, error message : ' . $resultset->getMessage());
+ 			echo "SELECT * FROM ".$config->table_monitored." WHERE extra='' AND box_id=".$box_id." ORDER BY name ASC\n";			
+ 			for ($i=0;$count($resultset)>$i;$i++)
  				{
-  					$var_name=$row['name'];
+  					$var_name=$resultset[$i]['name'];
   					preg_match("/".$var_name." = ([0-9]*)/i", $stats, $regs);
   					$var_value=$regs[1];
   					if ($var_value==NULL) $var_value="0"; 
-	    			mysql_query("INSERT INTO ".$config->table_monitoring." (name,value,time,box_id) VALUES ('".$var_name."','".$var_value."','".$time."',".$box_id.")") or die(mysql_error());
+	    			$sql = "INSERT INTO ".$config->table_monitoring." (name,value,time,box_id) VALUES ('".$var_name."','".$var_value."','".$time."',".$box_id.")";
+				$result = $link->prepare($sql);
+				$result->execute();
+				$result->free();
  				echo "INSERT INTO ".$config->table_monitoring." (name,value,time,box_id) VALUES ('".$var_name."','".$var_value."','".$time."',".$box_id.")\n";
  				}
 
  		echo("* ");
 	}
-	db_close();
 	
 	}
 $box_id++;
