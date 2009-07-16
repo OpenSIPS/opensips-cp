27751237b353f5a4cb6c063fafaceea355805bce
iulia_bublea <iulia@opensips.org>
2009-07-16 16:54:29 +0000

    Load balancer modue\le files added

---
+++ web/tools/loadbalancer/lib/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+require_once("../../../config/tools/loadbalancer/db.inc.php");
+require_once("../../../config/db.inc.php");
+
+	require_once("MDB2.php");
+        global $config;
+        if (isset($config->db_host_loadbalancer) && isset($config->db_user_loadbalancer) && isset($config->db_name_loadbalancer) ) {
+                $config->db_host = $config->db_host_loadbalancer;
+                $config->db_port = $config->db_port_loadbalancer;
+                $config->db_user = $config->db_user_loadbalancer;
+                $config->db_pass = $config->db_pass_loadbalancer;
+                $config->db_name = $config->db_name_loadbalancer;
+        }
+        $dsn = $config->db_driver.'://' . $config->db_user.':'.$config->db_pass . '@' . $config->db_host . '/'. $config->db_name.'';
+        $link = & MDB2::connect($dsn);
+        $link->setFetchMode(MDB2_FETCHMODE_ASSOC);
+        if(PEAR::isError($link)) {
+            die("Error while connecting : " . $link->getMessage());
+        }
+?>
---
+++ web/tools/loadbalancer/lib/loadbalancer.main.js
@@ -0,0 +1,21 @@
+<script language="JavaScript">
+//
+// $Id: nathelper.main.js 28 2009-04-01 15:27:03Z iulia_bublea $
+//
+  
+function confirmDelete()
+{
+ var agree=confirm("Are you sure you want to delete this entry?");
+ if (agree)	return true;
+  else return false;
+}
+
+function confirmDeleteRTPproxy()
+{
+ var agree=confirm("Are you sure you want to delete this Load Balancer definition?");
+ if (agree)	return true;
+  else return false;
+}
+
+
+</script>
