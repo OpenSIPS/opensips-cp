f4c9116de7587c145f24c68fd85a14cb26624738
iulia_bublea <iulia@opensips.org>
2009-09-14 12:44:37 +0000

    Modified local.inc.php

--- config/tools/nathelper/local.inc.php
+++ config/tools/nathelper/local.inc.php
@@ -6,20 +6,6 @@
  $config->results_per_page = 10;
  $config->results_page_range = 5;
  
- # Gateways
- // default gateway type
- $config->default_gw_type = 1;
- 
- 
- # Rules
- // "static" (from file) or "dynamic" (from table) group ids 
- $config->group_id_method = "static";
- 
- 
- # Groups
- // default domain
- $config->default_domain = "yourdomain.net";
-
 ###############################################################################
 
  //database tables
