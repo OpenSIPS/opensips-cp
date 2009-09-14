a444e411b592e2f74b1ed2fc0f8be3e780cfca43
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-09-14 12:43:20 +0000

    Parameters added for fifo type of data transmission.

--- branches/2.0/config/tools/siptrace/local.inc.php
+++ branches/2.0/config/tools/siptrace/local.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Id:$
+ * $Id$
  * Copyright (C) 2008 Voice Sistem SRL
  *
  * This file is part of opensips-cp, a free Web Control Panel Application for 
@@ -40,5 +40,8 @@
  
  // sip proxy - ip:port
  $proxy_list=array("udp:192.168.2.134:5060","udp:127.0.0.1:5060");
+
+ $config->reply_fifo_filename = "webfifo_".rand();
+ $config->reply_fifo_path = "/tmp/".$config->reply_fifo_filename;
  
 ?>
