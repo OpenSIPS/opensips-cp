ad9742a8d1ed2c8ca1cce1b98632456f66044aea
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-09-28 14:02:50 +0000

    Fixed bug in db_connect.php

--- trunk/web/tools/cdrviewer/lib/db_connect.php
+++ trunk/web/tools/cdrviewer/lib/db_connect.php
@@ -4,12 +4,12 @@ require_once("../../../config/db.inc.php");
 require_once("MDB2.php");
 
         global $config;
-        if (isset($config->db_host_dialplan) && isset($config->db_user_dialplan) && isset($config->db_name_dialplan) ) {
-                $config->db_host = $config->db_host_dialplan;
-                $config->db_port = $config->db_port_dialplan;
-                $config->db_user = $config->db_user_dialplan;
-                $config->db_pass = $config->db_pass_dialplan;
-                $config->db_name = $config->db_name_dialplan;
+        if (isset($config->db_host_cdrviewer) && isset($config->db_user_cdrviewer) && isset($config->db_name_cdrviewer) ) {
+                $config->db_host = $config->db_host_cdrviewer;
+                $config->db_port = $config->db_port_cdrviewer;
+                $config->db_user = $config->db_user_cdrviewer;
+                $config->db_pass = $config->db_pass_cdrviewer;
+                $config->db_name = $config->db_name_cdrviewer;
         }
         $dsn = $config->db_driver.'://' . $config->db_user.':'.$config->db_pass . '@' . $config->db_host . '/'. $config->db_name.'';
         $link = & MDB2::connect($dsn);
