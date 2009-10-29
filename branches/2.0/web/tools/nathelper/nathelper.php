055aa473bceb7c724fb293bc2ec9692a59d0e26d
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-10-29 14:17:36 +0000

    Bug fixed.

--- branches/2.0/web/tools/nathelper/nathelper.php
+++ branches/2.0/web/tools/nathelper/nathelper.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* $Id:$
+* $Id$
 * Copyright (C) 2008 Voice Sistem SRL
 *
 * This file is part of opensips-cp, a free Web Control Panel Application for
@@ -24,6 +24,7 @@
 require("template/header.php");
 require("lib/".$page_id.".main.js");
 require ("../../common/mi_comm.php");
+require("../../../config/db.inc.php");
 
 $table=$config->table_nathelper;
 $current_page="current_page_nathelper";
