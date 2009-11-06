0260482ce272f3f7cf2cef0bb52fc0f6ca6c19ae
iulia_bublea <iulia_bublea@92ccfc85-6c53-441d-8d1b-8961b210531c>
2009-11-06 15:05:42 +0000

    siptrace local.inc.php file modified

--- branches/3.0/config/tools/siptrace/local.inc.php
+++ branches/3.0/config/tools/siptrace/local.inc.php
@@ -40,5 +40,7 @@
  
  // sip proxy - ip:port
  $proxy_list=array("udp:192.168.2.134:5060","udp:127.0.0.1:5060");
- 
+
+ $config->reply_fifo_filename="webfifo_".rand();
+ $config->reply_fifo_path="/tmp/".$config->reply_fifo_filename; 
 ?>
