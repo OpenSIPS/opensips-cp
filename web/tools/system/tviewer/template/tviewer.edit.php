eda4c95aa3e4493548590fdf00de3a215c335b7c
untiptun <untiptun@users.noreply.github.com>
2010-05-13 15:42:23 +0000

    New TOOL : Table Viewer & Handler - You can set any table to be seen & managed from this tool

---
+++ web/tools/system/tviewer/template/tviewer.edit.php
@@ -0,0 +1,65 @@
+<form action="<?=$page_name?>?action=modify&id=<?=$_GET['id']?>" method="post">
+<table width="400" cellspacing="2" cellpadding="2" border="0">
+<?php
+/*
+ * $Id: dispatcher.edit.php 133 2009-10-29 18:05:56Z iulia_bublea $
+ * Copyright (C) 2008 Voice Sistem SRL
+ *
+ * This file is part of opensips-cp, a free Web Control Panel Application for 
+ * OpenSIPS SIP server.
+ *
+ * opensips-cp is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * opensips-cp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+ if (isset($form_error)) {
+                          echo(' <tr align="center">');
+                          echo('  <td colspan="2" class="dataRecord"><div class="formError">'.$form_error.'</div></td>');
+                          echo(' </tr>');
+                         }
+	$id=$_GET['id'];
+	
+	$sql = "select * from ".$table." where ".$config->custom_table_primary_key."='".$id."'";
+	$resultset = $link->queryAll($sql);
+    $index_row=0;
+	$link->disconnect();
+
+	
+
+?>
+<table width="400" cellspacing="2" cellpadding="2" border="0">
+ <tr align="center">
+  <td colspan="2" class="dispatcherTitle">Edit Record</td>
+ </tr>
+<?php
+	foreach ($config->custom_table_columns as $key => $value){
+ 
+?>
+ <tr>
+  <td class="dataRecord"><b><?php echo $key; ?>:</b></td>
+  <td class="dataRecord" width="275"><input type="text" name="<?php echo $value; ?>" value="<?=$resultset[0][$value]?>" maxlength="128" class="dataInput"></td>
+  </tr>
+
+<?php } ?>
+
+ <tr>
+  <td colspan="2" class="dataRecord" align="center"><input type="submit" name="save" value="Save" class="formButton"></td>
+ </tr>
+ <tr height="10">
+  <td colspan="2" class="dataTitle"><img src="images/spacer.gif" width="5" height="5"></td>
+ </tr>
+</table>
+</form>
+<?=$back_link?>
+
