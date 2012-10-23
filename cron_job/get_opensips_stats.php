6b255bcbfd7de224508a56460cec89080c5f0f91
untiptun <untiptun@users.noreply.github.com>
2012-10-23 11:46:40 +0000

    Removed prepared statements when inserting stats to DB

--- cron_job/get_opensips_stats.php
+++ cron_job/get_opensips_stats.php
@@ -41,36 +41,33 @@ $comm_type="";
 
 foreach ($boxes as $ar){
 
-	if ($ar['smonitor']['charts']==1)
-	{
+	if ($ar['smonitor']['charts']==1){
 		$time=time();
 		$sampling_time=get_config_var('sampling_time',$box_id);
 
-	//	if (date("i",$time) % $sampling_time == 0)
-		{
-			$comm_type=params($ar['mi']['conn']); 		
- 			$stats=get_all_vars();
-	
-			$sql = "SELECT * FROM ".$config->table_monitored." WHERE extra='' AND box_id=".$box_id." ORDER BY name ASC";
-			$resultset = $link->queryAll($sql);
-			if(PEAR::isError($resultset))
-		                die('Failed to issue query, error message : ' . $resultset->getMessage());
- 			for ($i=0;count($resultset)>$i;$i++)
- 				{
-  					$var_name=$resultset[$i]['name'];
-  					preg_match("/".$var_name." = ([0-9]*)/i", $stats, $regs);
-  					$var_value=$regs[1];
-  					if ($var_value==NULL) $var_value="0"; 
-	    			$sql = "INSERT INTO ".$config->table_monitoring." (name,value,time,box_id) VALUES ('".$var_name."','".$var_value."','".$time."',".$box_id.")";
-				$result = $link->prepare($sql);
-				$result->execute();
-				$result->free();
- 				}
+		$comm_type=params($ar['mi']['conn']); 		
+		$stats=get_all_vars();
 
+		$sql = "SELECT * FROM ".$config->table_monitored." WHERE extra='' AND box_id=".$box_id." ORDER BY name ASC";
+		$resultset = $link->queryAll($sql);
+
+		if(PEAR::isError($resultset)){
+			die('Failed to issue query, error message : ' . $resultset->getMessage());
+		}
+
+		for ($i=0;count($resultset)>$i;$i++){
+				$var_name=$resultset[$i]['name'];
+				preg_match("/".$var_name." = ([0-9]*)/i", $stats, $regs);
+				$var_value=$regs[1];
+				if ($var_value==NULL) $var_value="0"; 
+			$sql = "INSERT INTO ".$config->table_monitoring." (name,value,time,box_id) VALUES ('".$var_name."','".$var_value."','".$time."',".$box_id.")";
+			$result = $link->exec($sql);
+			if(PEAR::isError($result)) {
+				die('Failed to issue query, error message : ' . $resultset->getMessage());
+			}
+		}
 	}
-	
-	}
-$box_id++;
+	$box_id++;
 } 
 
 ?>
