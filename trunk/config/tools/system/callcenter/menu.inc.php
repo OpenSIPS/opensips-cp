4ca524a776b694173fbe720324923e69b5a15fc2
untiptun <untiptun@92ccfc85-6c53-441d-8d1b-8961b210531c>
2014-06-23 10:27:25 +0000

    New callcenter module

---
+++ trunk/config/tools/system/callcenter/menu.inc.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (C) 2014 OpenSIPS Project
+ *
+ * This file is part of opensips-cp, a free Web Control Panel Application for 
+ * OpenSIPS SIP server.
+ *
+ * opensips-cp is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * opensips-cp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+$module_id = "agents";
+
+$custom_config[$module_id]['menu_items'] = array(
+    array(
+          "dispatcher.php", // page name
+          "DB List" // menu name
+    )
+ );
+?>
