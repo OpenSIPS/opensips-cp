949d31ac4dede66d169683c365874bcc0db4d8f3
untiptun <untiptun@92ccfc85-6c53-441d-8d1b-8961b210531c>
2014-06-23 12:05:37 +0000

    changed nathelper config

--- trunk/config/tools/system/nathelper/menu.inc.php
+++ trunk/config/tools/system/nathelper/menu.inc.php
@@ -25,14 +25,6 @@
     array(
           "nathelper.php", // page name
           "DB List" // menu name
-    ),
-    array(
-          "natall.php", // page name
-          "Cache List" // menu name
-    ),
-    array(
-          "natping.php", // page name
-          "Nat Ping" // menu name
-   )
+    )
  );
 ?>
