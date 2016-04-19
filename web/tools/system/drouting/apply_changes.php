83aa1282a7f4e044082c86aae0ad49eaa2efe0bd
Bogdan-Andrei Iancu <bogdan@opensips.org>
2016-04-19 22:42:58 +0200

    Fixed broken include (related to previous work)

--- web/tools/system/drouting/apply_changes.php
+++ web/tools/system/drouting/apply_changes.php
@@ -30,7 +30,6 @@
 require("../../../../config/tools/system/drouting/local.inc.php");
 require("../../../common/mi_comm.php");
 require("../../../common/cfg_comm.php");
-require("lib/functions.inc.php");
 
 $command="dr_reload";
 
