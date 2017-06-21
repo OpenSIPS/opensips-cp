75ffa5d2f91fa71100d4dfea29c8322d13d106e7
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-06-21 15:41:44 +0200

    Fix syntax error (related to the prev cleanup)

--- web/tools/users/user_management/template/user_management.main.php
+++ web/tools/users/user_management/template/user_management.main.php
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
->
+?>
 
 <div id="dialog" class="dialog" style="display:none"></div>
 <div onclick="closeDialog();" id="overlay" style="display:none"></div>
