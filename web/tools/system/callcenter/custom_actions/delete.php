9fd9c4e6c0a0c0ad2b95593c4e9dc4b6f0aba2a0
Bogdan-Andrei Iancu <bogdan@opensips.org>
2017-06-28 16:54:33 +0200

    Removed old bogus code

--- web/tools/system/callcenter/custom_actions/delete.php
+++ web/tools/system/callcenter/custom_actions/delete.php
@@ -13,20 +13,6 @@ if ($action=="delete"){
 		$sql = "DELETE FROM ".$table." WHERE ".$custom_config[$module_id][$_SESSION[$module_id]['submenu_item_id']]['custom_table_primary_key']."=".$id;
 		$link->exec($sql);
 
-		
-
-		//put credit 0 in cassandra	
-		$query_credit = "delete from ppcredit where KEY='ba_credit_".$account_id."';";
-		$query_fm = "delete from ppcredit where KEY='ba_fm_".$account_id."';";
-
-		try {
-			$res = $link_cass->exec($query_credit);
-			$res = $link_cass->exec($query_fm);
-		}
-		catch (PDOException $e){
-			$errors = "Unable to update cassandra DB";
-		}
-
 	}else{
 
 		$errors= "User with Read-Only Rights";
