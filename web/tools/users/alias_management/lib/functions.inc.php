96e64d94eb4fcb570e8b3a806ecb65fc411c7ba0
adivjoseph <martin.horne@arm.com>
2017-11-04 17:53:45 -0700

    functions.inc.php
    remove error "Undefined property: stdClass::$table_aliases"

--- web/tools/users/alias_management/lib/functions.inc.php
+++ web/tools/users/alias_management/lib/functions.inc.php
@@ -28,7 +28,6 @@ function print_domains($type,$value,$has_any)
 
 	require("../../../../config/db.inc.php");
 	require("../../../../config/tools/system/domains/local.inc.php");
-        require("../../../../config/db.inc.php");
 	require("../../../../config/tools/users/user_management/local.inc.php");
         require("../../../../config/tools/system/domains/db.inc.php");
         require("db_connect.php");
