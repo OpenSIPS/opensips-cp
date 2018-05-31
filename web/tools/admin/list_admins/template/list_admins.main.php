c2429dfbfa842751bc64e555a1ec55468b9c681f
root <root@opensips.org>
2018-05-31 16:51:25 +0200

    Fix bottom spacer

--- web/tools/admin/list_admins/template/list_admins.main.php
+++ web/tools/admin/list_admins/template/list_admins.main.php
@@ -60,7 +60,7 @@ if(!$_SESSION['read_only']){
   value="<?=$search_lname?>" maxlength="32" class="searchInput"></td>
  </tr>
   <tr height="10">
-  <td colspan="2" class="searchRecord" align="center">
+  <td colspan="2" class="searchRecord border-bottom-devider" align="center">
   <input type="submit" name="search" value="Search" class="searchButton">&nbsp;&nbsp;&nbsp;
   <input type="submit" name="show_all" value="Show All" class="searchButton"></td>
  </tr>
