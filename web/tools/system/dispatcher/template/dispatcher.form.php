23cef633280aa92043d72cf89d67e3801b5104c3
Bogdan Iancu <bogdan@opensips.org>
2019-07-18 13:37:35 +0200

    Moved validation regexp for sockets into the "forms" file for reusage purposes

--- web/tools/system/dispatcher/template/dispatcher.form.php
+++ web/tools/system/dispatcher/template/dispatcher.form.php
@@ -60,7 +60,7 @@ form_generate_input_text("Destination", "SIP URI pointing to the destination",
 	"destination", "n", $ds_form['destination'], 192, "^sip:([^@]+@)?[^:]+(:[0-9]+)?$");
 
 form_generate_input_text("Socket", "The OpenSIPS network listener (as proto:ip:port) to be used for reaching this destination (leave empty if not needed)",
-	"socket", "y", $ds_form['socket'], 128, "^([a-zA-Z]+:)?([0-9]{1,3}\\\.[0-9]{1,3}\\\.[0-9]{1,3}\\\.[0-9]{1,3})(:[0-9]+)?$");
+	"socket", "y", $ds_form['socket'], 128, $re_socket);
 
 form_generate_select("State", "The intial state (active or inactive) of the destination",
 	"state", 200, $ds_form['state'], array("0","1"),array("Active","Inactive"));
