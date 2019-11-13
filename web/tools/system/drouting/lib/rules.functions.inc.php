OpenSIPS/opensips-cp

commit e0642920fd4a9c674b3d19bc43e0f11d8a807d54

drouting: rework group printing

web/tools/system/drouting/lib/rules.functions.inc.php
@@ -21,26 +21,6 @@
  */
 
 
-function get_groupids()
-{
- global $config;
- $index = 0;
- $values = array();
- $sql="select distinct groupid from ".$config->table_groups." order by groupid asc";
- $stm = $link->prepare($sql);
- if ($stm === false) {
- 	die('Failed to issue query ['.$sql.'], error message : ' . print_r($link->errorInfo(), true));
- }
- $stm->execute( array() );
- $result= $stm->fetchAll(PDO::FETCH_ASSOC);
- for($i=0;count($result)>$i;$i++)
- {
-  $values[$index] = $result[$i]['groupid'];
-  $index++;
- }
- return($values);
-}
-
 function get_gwlist()
 {
  //include("db_connect.php");
@@ -88,59 +68,49 @@ function get_carrierlist()
  return($values);
 }
 
+function get_groupids()
+{
+	global $config;
+	$i = 0;
+	$values = array();
+	if ($config->group_id_method=="static") {
+		$filename = "../../../../config/tools/system/drouting/group_ids.txt";
+		$handle = fopen($filename, "r");
+		while (!feof($handle))
+		{
+			$buffer = fgets($handle, 4096);
+			$pos = strpos($buffer, " ");
+			$value = trim(substr($buffer, 0, $pos));
+			if ($value == "")
+				continue;
+			$descr = trim(substr($buffer, $pos, strlen($buffer)));
+			if ($descr == "")
+				$descr = $value;
+			$values[$i]['groupid'] = $value;
+			$values[$i]['description'] = $descr;
+			$i++;
+		}
+		fclose($handle);
+	} else {
+		global $link;
+		$sql="select distinct groupid,description from ".$config->table_groups." order by groupid asc";
+		$stm = $link->prepare($sql);
+		if ($stm===FALSE) {
+			die('Failed to issue query ['.$sql.'], error message : ' . $link->errorInfo()[2]);
+		}
+		$stm->execute(array());
+		$values = $stm->fetchAll(PDO::FETCH_ASSOC);
+	}
+	return $values;
+}
 
 function print_groupids()
 {
- global $config;
- if ($config->group_id_method=="static") {
-  $filename = "../../../../config/tools/system/drouting/group_ids.txt";
-  $handle = fopen($filename, "r");
-  while (!feof($handle))
-  {
-   $buffer = fgets($handle, 4096);
-   $pos = strpos($buffer, " ");
-   $values[] = trim(substr($buffer, 0, $pos));
-   $content[] = trim(substr($buffer, $pos, strlen($buffer)));
-  }
-  fclose($handle);
-  echo('<select name="groupid_value" id="groupid_value" size="1" class="dataSelect" style="width:420px!important; margin-left:1px;margin-top:2px;">');
-  for ($i=0; $i<sizeof($values); $i++)
-   if($values[$i]!="")
-	echo('<option value="'.$values[$i].'">'.$values[$i].' - '.$content[$i].'</option>');
-  echo('</select>');
- }
- if ($config->group_id_method=="dynamic") {
-  $values = get_groupids();
-  sort($values);
-  echo('<select name="groupid_value" id="groupid_value" size="1" class="dataSelect" style="width:230px; margin-left:1px; margin-top:2px;">');
-  for ($i=0; $i<sizeof($values); $i++)
-   echo('<option value="'.$values[$i].'">'.$values[$i].'</option>');
-  echo('</select>');
- }
-}
-
-function get_groups($list)
-{
- $group = explode(",", $list);
- $filename = "../../../../config/tools/system/drouting/group_ids.txt";
- $handle = fopen($filename, "r");
- while (!feof($handle))
- {
-  $buffer = fgets($handle, 4096);
-  $pos = strpos($buffer, " ");
-  $value[] = trim(substr($buffer, 0, $pos));
-  $content[] = trim(substr($buffer, $pos, strlen($buffer))); 
- }
- fclose($handle);
- $result="";
- for ($i=0; $i<sizeof($group); $i++)
- {
-  for ($j=0; $j<sizeof($value); $j++)
-   if ($value[$j]!="" && $value[$j]==$group[$i]) $result.=$value[$j]." - ".$content[$j].", ";
- }
- $n = strlen($result);
- echo(substr($result, 0, $n-2));
- return;
+	$values = get_groupids();
+	echo('<select name="groupid_value" id="groupid_value" size="1" class="dataSelect" style="width:420px!important; margin-left:1px;margin-top:2px;">');
+	for ($i=0; $i<sizeof($values); $i++)
+		echo('<option value="'.$values[$i]['groupid'].'">'.$values[$i]['groupid'].' - '.$values[$i]['description'].'</option>');
+	echo('</select>');
 }
 
 function print_gwlist()

web/tools/system/drouting/lib/carriers.functions.inc.php
@@ -35,25 +35,6 @@ function fmt_binary($x, $numbits, $retbit) {
 	    return ltrim($rtnval[$retbit]);
 } 
 
-function get_groupids()
-{
- global $config;
- $index = 0;
- $values = array();
- $sql="select distinct groupid from ".$config->table_groups." order by groupid asc";
- $stm = $link->prepare($sql);
- if ($stm===FALSE) {
-	die('Failed to issue query ['.$sql.'], error message : ' . $link->errorInfo()[2]);
- }
- $stm->execute( array() );
- $resultset = $stm->fetchAll(PDO::FETCH_ASSOC);
- for($i=0;count($resultset)>$i;$i++)
- {
-  $values[$index] = $resultset[$i]['groupid'];
-  $index++;
- }
- return($values);
-}
 
 function get_gwlist()
 {
@@ -77,60 +58,6 @@ function get_gwlist()
   $index++;
  }
  return($values);
-}
-
-function print_groupids()
-{
- global $config;
- if ($config->group_id_method=="static") {
-  $filename = "../../../../config/tools/system/drouting/group_ids.txt";
-  $handle = fopen($filename, "r");
-  while (!feof($handle))
-  {
-   $buffer = fgets($handle, 4096);
-   $pos = strpos($buffer, " ");
-   $values[] = trim(substr($buffer, 0, $pos));
-   $content[] = trim(substr($buffer, $pos, strlen($buffer)));
-  }
-  fclose($handle);
-  echo('<select name="groupid_value" id="groupid_value" size="1" class="dataSelect" style="width:420px!important; margin-left:1px;margin-top:2px;">');
-  for ($i=0; $i<sizeof($values); $i++)
-   if($values[$i]!="")
-	echo('<option value="'.$values[$i].'">'.$values[$i].' - '.$content[$i].'</option>');
-  echo('</select>');
- }
- if ($config->group_id_method=="dynamic") {
-  $values = get_groupids();
-  sort($values);
-  echo('<select name="groupid_value" id="groupid_value" size="1" class="dataSelect">');
-  for ($i=0; $i<sizeof($values); $i++)
-   echo('<option value="'.$values[$i].'">'.$values[$i].'</option>');
-  echo('</select>');
- }
-}
-
-function get_groups($list)
-{
- $group = explode(",", $list);
- $filename = "../../../../config/tools/system/drouting/group_ids.txt";
- $handle = fopen($filename, "r");
- while (!feof($handle))
- {
-  $buffer = fgets($handle, 4096);
-  $pos = strpos($buffer, " ");
-  $value[] = trim(substr($buffer, 0, $pos));
-  $content[] = trim(substr($buffer, $pos, strlen($buffer))); 
- }
- fclose($handle);
- $result="";
- for ($i=0; $i<sizeof($group); $i++)
- {
-  for ($j=0; $j<sizeof($value); $j++)
-   if ($value[$j]!="" && $value[$j]==$group[$i]) $result.=$value[$j]." - ".$content[$j].", ";
- }
- $n = strlen($result);
- echo(substr($result, 0, $n-2));
- return;
 }
 
 function print_gwlist()

web/tools/system/drouting/template/rules.add.php
@@ -53,7 +53,7 @@
   <td class="dataRecord"><input type="text" name="prefix" value="<?=$prefix?>"  style="width:230px;" maxlength="64" class="dataInput"></td>
  </tr>
  <tr>
-  <td class="dataRecord">Time Recurrence<br><center><img src="../../../images/share/info.png" onMouseOver="this.style.cursor='pointer'" onClick="window.open('info.html','info','width=500,height=400')"></center></td>
+  <td class="dataRecord">Time Recurrence<!--<br><center><img src="../../../images/share/info.png" onMouseOver="this.style.cursor='pointer'" onClick="window.open('info.html','info','width=500,height=400')"></center>--></td>
   <td class="dataRecord">
   
    <select name="time_recurrence" class="dataSelect" id="time_recurrence" style="width:230px;"  onChange="optionChange('time_recurrence')">

web/tools/system/drouting/template/rules.edit.php
@@ -54,7 +54,7 @@
   <td class="dataRecord"><input type="text" name="prefix" value="<?=$resultset[0]['prefix']?>" style="width:230px;" maxlength="64" class="dataInput"></td>
  </tr>
  <tr>
-  <td class="dataRecord">Time Recurrence<br><br><center><img src="../../../images/share/info.png" onMouseOver="this.style.cursor='pointer'" onClick="window.open('info.html','info','width=500,height=400')"></center></td>
+  <td class="dataRecord">Time Recurrence<!--<br><center><img src="../../../images/share/info.png" onMouseOver="this.style.cursor='pointer'" onClick="window.open('info.html','info','width=500,height=400')"></center>--></td>
   <td class="dataRecord">
    
    <?=parse_timerec($resultset[0]['timerec'],0)?><hr>

commit 5613750e0a0afef9ad441b61bfba5b391aa0968b

drouting: fix rules group details
Bug introduced in commit e0642920fd

web/tools/system/drouting/lib/rules.functions.inc.php
@@ -68,6 +68,16 @@ function get_carrierlist()
  return($values);
 }
 
+function get_groupid($group)
+{
+	$values = get_groupids();
+	for ($i=0; $i<sizeof($values); $i++)
+		if ($values[$i]['groupid'] == $group)
+			return $values[$i]['groupid'].' - '.$values[$i]['description'];
+	return "";
+}
+
+
 function get_groupids()
 {
 	global $config;

web/tools/system/drouting/template/rules.details.php
@@ -36,8 +36,7 @@
  <tr>
   <td class="dataRecord"><b>Group ID:</b> 
   <?php 
-   if ($config->group_id_method=="static") get_groups($resultset[0]['groupid']);
-   if ($config->group_id_method=="dynamic") echo($resultset[0]['groupid']);
+   echo (get_groupid($resultset[0]['groupid']));
   ?>
   </td>
  </tr>