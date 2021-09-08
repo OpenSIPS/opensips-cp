79be8040de3bc75583db3131dcc43d4c3e60b602
root <root@localhost.localdomain>
2021-09-08 09:15:18 +0000

    Add params for callcenter

---
+++ web/tools/system/callcenter/params.php
@@ -0,0 +1,10 @@
+<?php
+
+$config->callcenter = array(
+    "talk_to_this_assoc_id" => array(
+		"default" => 1,
+		"name"    => "Talk to this assoc id",
+		"options" => get_assoc_id(),
+		"type"    => "dropdown",
+	),
+);
