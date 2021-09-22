6b0e095f522e5e4519dbbe4de6441f7c5f1faf9e
root <root@localhost.localdomain>
2021-09-22 12:26:52 +0000

    Fix order of requirements in drouting tool

--- web/tools/system/drouting/gateways.php
+++ web/tools/system/drouting/gateways.php
@@ -20,10 +20,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+ require("../../../common/cfg_comm.php");
  require("template/header.php");
  include("lib/db_connect.php");
  require ("../../../common/mi_comm.php");
- require("../../../common/cfg_comm.php");
  require("../../../../config/db.inc.php");
  $table=$config->table_gateways;
  $current_page="current_page_gateways";
