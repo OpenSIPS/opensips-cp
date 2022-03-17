3ef339d9705abfb471e09d4a7654fb33b30fe75e
Bogdan Iancu <bogdan@opensips.org>
2022-03-17 09:22:29 +0200

    [domains] Enhance settings definitions
    - group them in categories (using titles)

--- config/tools/system/domains/settings.inc.php
+++ config/tools/system/domains/settings.inc.php
@@ -21,18 +21,27 @@
  */
 
 $config->domains = array(
-    "table_domains" => array(
-        "default" => "domains",
-        "name" => "Table Domains",
-        "type" => "text",
-		"tip"  => "The database table name for storing the domain entries"
-    ),
+	"title0" => array(
+		"type" => "title",
+		"title" => "General settings"
+	),
 	"talk_to_this_assoc_id" => array(
 		"default" => 1,
-		"name"    => "Talk to this assoc id",
+		"name"    => "Linked system",
 		"options" => get_assoc_id(),
 		"type"    => "dropdown",
 		"tip" 	  => "As OCP can manage multiple OpenSIPS instances, this is the association 
 		ID pointing to the group of servers (system) which needs to be provision with this domain information."
-	)
+	),
+
+	"title1" => array(
+		"type" => "title",
+		"title" => "DB settings"
+	),
+	"table_domains" => array(
+		"default" => "domains",
+		"name" => "Table Domains",
+		"type" => "text",
+		"tip"  => "The database table name for storing the domain entries"
+	),
 );
