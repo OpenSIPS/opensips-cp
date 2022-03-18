ad53ad631cd988ec73b0d53bfff6927e945dc908
Bogdan Iancu <bogdan@opensips.org>
2022-03-18 13:26:04 +0200

    [clusterer] Enhance settings definitions
    - group them in categories (using titles)

--- config/tools/system/clusterer/settings.inc.php
+++ config/tools/system/clusterer/settings.inc.php
@@ -21,19 +21,27 @@
  */
 
 $config->clusterer = array(
-    "talk_to_this_assoc_id" => array(
+	"title0" => array(
+		"type" => "title",
+		"title" => "General settings"
+	),
+	"talk_to_this_assoc_id" => array(
 		"default" => 1,
-		"name"    => "Talk to this assoc id",
+		"name"    => "Linked system",
 		"options" => get_assoc_id(),
 		"type"    => "dropdown",
-        "tip"     => "As OCP can manage multiple OpenSIPS instances, this is the association ID
-         pointing to the group of servers (system) which needs to be provision with this clusterer information.",
+		"tip"     => "As OCP can manage multiple OpenSIPS instances, this is the association ID
+		pointing to the group of servers (system) which needs to be provision with this clusterer information.",
 	),
-    "table_clusterer" => array(
-        "default" => "clusterer",
-        "name" => "Table Clusterer",
-        "tip" => "The name of the DB table holding the cluster configuration 
-        (this needs to be correlated with the OpenSIPS configuration). The default value is 'clusterer'.",
-        "type" => "text"
-    )
+	"title1" => array(
+		"type" => "title",
+		"title" => "DB settings"
+	),
+	"table_clusterer" => array(
+		"default" => "clusterer",
+		"name" => "Table Clusterer",
+		"tip" => "The name of the DB table holding the cluster configuration 
+		(this needs to be correlated with the OpenSIPS configuration). The default value is 'clusterer'.",
+		"type" => "text"
+	)
 );
