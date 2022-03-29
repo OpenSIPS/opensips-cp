b0510ffeae0d9583923eecdb08eb295adf0d65e7
Bogdan Iancu <bogdan@opensips.org>
2022-03-29 18:39:32 +0300

    [permissions] Enhance settings definitions
    - group them in categories (using titles)
    - tune a bit the default values

--- config/tools/system/permissions/settings.inc.php
+++ config/tools/system/permissions/settings.inc.php
@@ -21,32 +21,46 @@
  */
 
 $config->permissions = array(
-    "results_per_page" => array(
-		"default" => 25,
-		"name"    => "Results per page",
-		"tip"    => "Number of results per page",
-		"type"    => "number",
+	"title0" => array(
+		"type" => "title",
+		"title" => "General settings"
+	),
+	"talk_to_this_assoc_id" => array(
+		"default" => 1,
+		"name"	=> "Linked system",
+		"options" => get_assoc_id(),
+		"type"	=> "dropdown",
+		"tip"	 => "As OCP can manage multiple OpenSIPS instances, this is the association 
+		ID pointing to the group of servers (system) which needs to be provision with this permissions information."
+	),
+
+	"title1" => array(
+		"type" => "title",
+		"title" => "DB settings"
+	),
+	"table_address" => array(
+		"default" => "address",
+		"name" => "Table Address",
+		"type" => "text",
+		"tip"  => "The database table name for storing the IP matching rules"
+	),
+
+	"title2" => array(
+		"type" => "title",
+		"title" => "Display settings"
+	),
+	"results_per_page" => array(
+		"default" => 30,
+		"name"	=> "Results per page",
+		"tip"	=> "Number of results per page",
+		"type"	=> "number",
 		"validation_regex" => "^[0-9]+$",
 	),
 	"results_page_range" => array(
 		"default" => 10,
-		"name"    => "Results page range",
-		"tip"    => "Control over the pagination when displaying the IP matching rules",
-		"type"    => "number",
+		"name"	=> "Results page range",
+		"tip"	=> "Control over the pagination when displaying the IP matching rules",
+		"type"	=> "number",
 		"validation_regex" => "^[0-9]+$",
-    ),
-    "talk_to_this_assoc_id" => array(
-		"default" => 1,
-		"name"    => "Talk to this assoc id",
-		"options" => get_assoc_id(),
-		"type"    => "dropdown",
-		"tip"     => "As OCP can manage multiple OpenSIPS instances, this is the association 
-		ID pointing to the group of servers (system) which needs to be provision with this permissions information."
 	),
-    "table_address" => array(
-        "default" => "address",
-        "name" => "Table Address",
-        "type" => "text",
-		"tip"  => "The database table name for storing the IP matching rules"
-    ),
 );
