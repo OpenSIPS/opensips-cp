5512b0ba36f1b692b44a52a9a25a7c5d5c5c5446
Bogdan Iancu <bogdan@opensips.org>
2022-03-29 19:17:55 +0300

    [homer] Enhance settings definitions
    - group them in categories (using titles)

--- config/tools/system/homer/settings.inc.php
+++ config/tools/system/homer/settings.inc.php
@@ -21,20 +21,24 @@
  */
 
 $config->homer = array(
-    "homer_URL" => array(
+	"title0" => array(
+		"type" => "title",
+		"title" => "General settings"
+	),
+	"homer_URL" => array(
 		"default" => "http://homer.opensips.org",
-		"name"    => "Homer URL",
-		"type"    => "text"
+		"name"	=> "Homer URL",
+		"type"	=> "text"
 	),
-    "homer_auth_method" => array(
+	"homer_auth_method" => array(
 		"default" => "get",
-		"name"    => "Homer authentication method",
-		"type"    => "text"
+		"name"	=> "Homer authentication method",
+		"type"	=> "text"
 	),
-    "common_subdomain" => array(
+	"common_subdomain" => array(
 		"default" => ".opensips.org",
-		"name"    => "Common subdomain",
-		"type"    => "text"
+		"name"	=> "Common subdomain",
+		"type"	=> "text"
 	),
-    
+	
 );
