06b459eed0beec06f53ac956874c291f90678478
Razvan Crainea <razvan@opensips.org>
2022-04-08 14:16:18 +0300

    dispatcher: proper report MI errors

--- web/tools/system/dispatcher/dispatcher.php
+++ web/tools/system/dispatcher/dispatcher.php
@@ -249,6 +249,8 @@ if ($action=="change_state") {
 	$mi_connectors=get_all_proxys_by_assoc_id(get_settings_value('talk_to_this_assoc_id'));
 	for ($i=0;$i<count($mi_connectors);$i++){
 	        $message=mi_command("ds_set_state", array("state"=>$desired_state,"group"=>$group,"address"=>$address),$mi_connectors[$i],$errors);
+		if ($errors)
+			$errors=join(", ", $errors);
 	}
 
 
