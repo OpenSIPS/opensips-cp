OpenSIPS/opensips-cp

commit bdbdbc94a785004460c171bbd90e0b64ab6fc686

addresses: add partition support

web/tools/system/addresses/apply_changes.php
@@ -34,14 +34,22 @@ $command="address_reload";
 <?php
 
 $mi_connectors=get_proxys_by_assoc_id(get_settings_value('talk_to_this_assoc_id'));
+$addresses_partition = get_settings_value("addresses_partition");
+if ($addresses_partition == "")
+	$addresses_partition = false;
 
 for ($i=0;$i<count($mi_connectors);$i++){
 	echo "Sending to <b>".$mi_connectors[$i]."</b> : ";
 
+	$errors = NULL;
 	$message=mi_command($command, NULL, $mi_connectors[$i], $errors);
+		($addresses_partition?array("partition"=>$addresses_partition):NULL),
+		$mi_connectors[$i], $errors);
 
 	if (empty($errors)) {
 		echo "<font color='green'><b>Success</b></font>";
+	} else {
+		echo '<tr><td align="center"><div class="formError">'.join(", ", $errors).'</div></td></tr>';
 	}
 	echo "<br>";
 }

config/tools/system/addresses/settings.inc.php
@@ -89,6 +89,15 @@ $config->addresses = array(
 }"
 	),
 
+	"address_partition" => array(
+		"default" => "",
+		"opt"     => "y",
+		"name"    => "Address partition",
+		"type"    => "text",
+		"tip"     => "The name of the address partition to work with; if empty, it will assume no partition support in OpenSIPS",
+		"validation_regex" => null,
+	),
+
 	"title3" => array(
 		"type" => "title",
 		"title" => "Display settings"

commit 5b27fa575ed3417609751dd1edd0a4e00335c50a

addresses: fix mi reload syntax error
Introduced in bdbdbc94a

web/tools/system/addresses/apply_changes.php
@@ -42,7 +42,7 @@ for ($i=0;$i<count($mi_connectors);$i++){
 	echo "Sending to <b>".$mi_connectors[$i]."</b> : ";
 
 	$errors = NULL;
-	$message=mi_command($command, NULL, $mi_connectors[$i], $errors);
+	$message=mi_command($command,
 		($addresses_partition?array("partition"=>$addresses_partition):NULL),
 		$mi_connectors[$i], $errors);
 