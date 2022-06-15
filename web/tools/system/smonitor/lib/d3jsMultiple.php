8aa0eee795469ce71cfe1d1599d918c475678f16
Airinei Daniel <airineidaniel23@gmail.com>
2022-06-15 07:04:43 +0000

    smonitor: revert proper size to charts

--- web/tools/system/smonitor/lib/d3jsMultiple.php
+++ web/tools/system/smonitor/lib/d3jsMultiple.php
@@ -42,8 +42,8 @@ function(d){
     var zoomTrigger = false;
     
   // set the dimensions and margins of the graph
-  var margin = {top: 0, right: 30, bottom: 100, left: 50},
-      width = 400 - margin.left - margin.right,
+  var margin = {top: 10, right: 30, bottom: 30, left: 50},
+      width = 660 - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
