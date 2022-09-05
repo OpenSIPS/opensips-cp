726a235dcee639bde2ba7fa1a01dd287af1a542e
Airinei Daniel <airineidaniel23@gmail.com>
2022-09-05 08:33:28 +0000

    Add mandatory filters

--- web/tools/system/tracer/tracer.php
+++ web/tools/system/tracer/tracer.php
@@ -47,7 +47,7 @@ if ($action == "start") {
 
 }
 echo ('
-<form action="tracer.php?action=start" method="post">
+<form action="tracer.php?actio	n=start" method="post">
 <table width="400" cellspacing="2" cellpadding="2" border="0" name="filters_table">
 <tr align="center">
 <td colspan="2" height="10" class="mainTitle">Set filters</td>
@@ -56,11 +56,12 @@ echo ('
 form_generate_input_text("Caller", "Caller", "caller_id", "y", (isset($_GET['action'])?$_POST['caller_id']:NULL), 100, NULL);
 form_generate_input_text("Callee", "Callee", "callee_id", "y", (isset($_GET['action'])?$_POST['callee_id']:NULL), 100, NULL);
 form_generate_input_text("IP", "IP address", "ip_id", "y", (isset($_GET['action'])?$_POST['ip_id']:NULL), 100, NULL);
+
 echo (' <tr>
 <td colspan="2">
   <table cellspacing=20>
 	<tr>
-	  <td class="dataRecord" align="left" width="50%"><input type="submit" name="setFilters" value="Start" class="formButton"></td>');
+	  <td class="dataRecord" align="left" width="50%"><input type="submit" name="setFilters" value="Start" class="formButton" disabled></td>');
 	  if ($action == "start") echo ('<td class="dataRecord" align="right" width="50%"><input onclick="window.location.href=\'tracer.php\';" class="formButton" value="Stop" type="button"></td>');
 echo ('
 	</tr>
@@ -100,7 +101,17 @@ if ($action == "start") {
 		exit;
 	}
 	$result = socket_bind($socket, $host, $port) or die(" bind fail\n");
-	$res = mi_command("trace_start", array("id" => $prefix.$random, "uri" => "hep:".$adv_ip.":".$adv_port.";transport=tcp;version=3"), $boxes[0]['mi_conn'], $errors);
+	$filter = [];
+	if (isset($_POST['caller_id'])) {
+		$filter['caller'] = $_POST['caller_id'];
+	}
+	if (isset($_POST['callee_id'])) {
+		$filter['callee'] = $_POST['callee_id'];
+	}
+	if (isset($_POST['ip_id'])) {
+		$filter['ip'] = $_POST['ip_id'];
+	}
+	$res = mi_command("trace_start", array("id" => $prefix.$random, "uri" => "hep:".$adv_ip.":".$adv_port.";transport=tcp;version=3", "filter" => $filter), $boxes[0]['mi_conn'], $errors);
 
 	$result = socket_listen($socket, 3) or die(" listen fail\n");
 	$i = 0;
