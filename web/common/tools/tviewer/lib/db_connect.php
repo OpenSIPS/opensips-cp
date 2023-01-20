f7dc677d55d6f6119f90211277429cffa8576b68
Razvan Crainea <razvan@opensips.org>
2023-01-20 16:50:23 +0200

    tviewer: consider db_config settings

--- web/common/tools/tviewer/lib/db_connect.php
+++ web/common/tools/tviewer/lib/db_connect.php
@@ -1,6 +1,19 @@
 <?php
 $module_id = $_SESSION['module_id'];
 $branch = $_SESSION['branch'];
+$db_config_submenu = "db_config_" . $_SESSION[$module_id]['submenu_item_id'];
+
+$configuration = null;
+if (get_settings_value_from_tool($db_config_submenu, $module_id))
+	$configuration = get_settings_value_from_tool($db_config_submenu, $module_id);
+elseif (get_settings_value_from_tool("db_config", $module_id))
+	$configuration = get_settings_value_from_tool("db_config", $module_id);
+else
+	$configuation = null;
+
+if ($configuration)
+	$configuration = $_SESSION['db_config'][$configuration];
+
 require_once("../../../../config/db.inc.php");
 require_once("../../../../config/tools/".$branch."/".$module_id."/db.inc.php");
 
@@ -15,6 +28,15 @@ if (isset($custom_config[$module_id][$_SESSION[$module_id]['submenu_item_id']]['
 
 	if (isset($config->db_port) && is_int((int)$config->db_port) && 1 < $config->db_port && $config->db_port < 65535) 
 		$config->db_host = $config->db_host.";port=".$config->db_port;
+} else if ($configuration && isset($configuration["db_host"]) && isset($configuration["db_user"]) && isset($configuration["db_name"])) {
+	$config->db_host = $configuration['db_host'];
+	$config->db_user = $configuration['db_user'];
+	$config->db_pass = isset($configuration['db_pass'])?$configuration['db_pass']:'';
+	$config->db_name = $configuration['db_name'];
+	$config->db_attr = $configuration['db_attr'];
+
+	if (isset($configuration->db_port) && is_int((int)$configuration->db_port) && 1 < $configuration->db_port && $configuration->db_port < 65535) 
+		$config->db_host = $config->db_host.";port=".$configuration->db_port;
 }
 
 $dsn = $config->db_driver . ':host=' . $config->db_host . ';dbname='. $config->db_name;
