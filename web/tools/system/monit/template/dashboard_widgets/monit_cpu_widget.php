7d2fbbbd720b728d69df7c62ba3655306b36d5d4
Razvan Crainea <razvan@opensips.org>
2023-02-03 19:10:34 +0200

    monit/widget: proper refresh and thresholds

--- web/tools/system/monit/template/dashboard_widgets/monit_cpu_widget.php
+++ web/tools/system/monit/template/dashboard_widgets/monit_cpu_widget.php
@@ -4,36 +4,97 @@ require_once(__DIR__."/../../../../system/dashboard/template/widget/widget.php")
 class monit_cpu_widget extends widget
 {
     public $cpu_res;
-	public $box_id;
-	public $service;
+    public $box_id;
+    public $service;
+    public $warning_threshold = 50;
+    public $critical_threshold = 70;
 
     function __construct($array) {
-        parent::__construct($array['panel_id'], $array['widget_name'], 1,2, $array['widget_name']);
-		$this->box_id = $array['widget_box'];
-		$this->service = $array['widget_service'];
-        $this->set_cpu();
+	if (isset($array['widget_refresh']) && $array['widget_refresh'] != '')
+		$r = intval($array['widget_refresh']) * 1000;
+	else
+		$r = 60000; # one minute is the default
+        parent::__construct($array['panel_id'], $array['widget_name'], 1,2, $array['widget_name'], $r);
+	$this->box_id = $array['widget_box'];
+	$this->service = $array['widget_service'];
+	if (isset($array['widget_critical']))
+		$this->critical_threshold = floatval($array['widget_critical']);
+	if (isset($array['widget_warning']))
+		$this->warning_threshold = floatval($array['widget_warning']);
+	// makes no sense to have warning larger than critical
+	if ($this->warning_threshold > $this->critical_threshold)
+		$this->warning_threshold = $this->critical_threshold;
+	$this->set_cpu();
+    }
+
+    function set_cpu_val(&$cpu, $val) {
+	    if ($val == "") {
+		    $cpu = array('n\a', widget::STATUS_CRIT);
+		    return;
+	    }
+	    $val = floatval($val);
+	    if ($val >= $this->critical_threshold)
+		    $status = widget::STATUS_CRIT;
+	    elseif ($val >= $this->warning_threshold)
+		    $status = widget::STATUS_WARN;
+	    else
+		    $status = widget::STATUS_OK;
+	    $cpu = array($val, $status);
+    }
+
+    function set_cpu_status() {
+	    # biggest status of both
+	    $this->set_status(max($this->cpu_usr[1], $this->cpu_sys[1]));
+    }
+
+    function get_cpu_val($cpu) {
+	    return $cpu[0];
+    }
+
+    function get_cpu_color($cpu) {
+	    switch ($cpu[1]) {
+	    case widget::STATUS_WARN:
+		    return "orange";
+	    case widget::STATUS_CRIT:
+		    return "red";
+	    case widget::STATUS_OK:
+	    default:
+		    return "black";
+	    }
+    }
+
+    function init_cpu() {
+	$this->set_cpu_val($this->cpu_usr, 'n\a');
+	$this->set_cpu_val($this->cpu_sys, 'n\a');
     }
 
     function get_name() {
         return "CPU monit widget";
     }
     function display_test() {
-		echo ('
+	    echo ('
 			<table style="table-layout: fixed;
 			width: 90px; height:20px; margin: auto; margin-left: 1px; font-weight: bolder;" cellspacing="2" cellpadding="2" border="0">
 			');
 		echo ('
-			<tr><td class="rowEven"><span style="color:black;">USR: </td><td>'.(isset($this->cpu_res['usr'])?$this->cpu_res['usr']:$this->cpu_res['us']).'%</span></td></tr>
-			<tr><td class="rowEven"><span style="color:black;">SYS: </td><td>'.(isset($this->cpu_res['sys'])?$this->cpu_res['sys']:$this->cpu_res['sy']).'%</span></td></tr>
+			<tr><td class="rowEven">USR: </td><td><span style="color:'.$this->get_cpu_color($this->cpu_usr).';">'.$this->get_cpu_val($this->cpu_usr).'%</span></td></tr>
+			<tr><td class="rowOdd">SYS: </td><td><span style="color:'.$this->get_cpu_color($this->cpu_sys).';">'.$this->get_cpu_val($this->cpu_sys).'%</span></td></tr>
 			</tr>  ');
 		echo('</table>');
-	
+
 	}
 
 	function set_cpu() {
+		$this->init_cpu();
+		$widget_box = null;
 		foreach ($_SESSION['boxes'] as $box) {
 			if ($box['id'] == $this->box_id)
 				$widget_box = $box;
+		}
+		if ($widget_box == null) {
+			$errors[] = "did not find box";
+			error_log("did not find box ".$this->box_id);
+			return false;
 		}
 		$auth_user = $widget_box['monit_user'];
 		$auth_pass = $widget_box['monit_pass'];
@@ -41,7 +102,6 @@ class monit_cpu_widget extends widget
 		if ($widget_box['monit_ssl'])
 			$protocol = "https";
 		$host = $widget_box['monit_conn'];
-
 
 		$auth = base64_encode($auth_user.":".$auth_pass);
 		$ch = curl_init();
@@ -66,36 +126,38 @@ class monit_cpu_widget extends widget
 
 		if ($status>=300) {
 			$errors[] = "MI HTTP request failed with ".$status." reply";
-			return NULL;
+			return false;
 		}
 
-		preg_match('/(cpu[\s\r\n]*(.\[[0-9];[0-9]*m)?(?<cpu_info>.*?)[\s\r\n]*memory)/s', $response, $matches);
+		preg_match('/(cpu[\s\r\n]*(.\[[0-9];[0-9]+m)?(?<cpu_info>.*?)[\s\r\n]*memory)/s', $response, $matches);
 		$cpu_info = utf8_encode($matches['cpu_info']);
-		preg_match_all('/(?<cpu_info>.*?) /', $cpu_info, $matches);
-		foreach($matches['cpu_info'] as $key => $entry) {
-			preg_match('/(?<value>([0-9]*\.[0-9])*)\%(?<name>.*)/', $entry, $new_matches);
-			$this->cpu_res[$new_matches['name']] = utf8_encode($new_matches['value']);
-		}
+		preg_match('/([0-9]*\.[0-9]*)\%usr? /', $cpu_info, $cpu_matches);
+		if (isset($cpu_matches[1]))
+			$this->set_cpu_val($this->cpu_usr, $cpu_matches[1]);
+
+		preg_match('/ ([0-9]*\.[0-9]*)\%sys? /', $cpu_info, $cpu_matches);
+		if (isset($cpu_matches[1]))
+			$this->set_cpu_val($this->cpu_sys, $cpu_matches[1]);
+		$this->set_cpu_status();
 	}
 
     function echo_content() {
         $this->display_test();
     }
 
-    function get_as_array() {
-        return array($this->get_html(), $this->get_sizeX(), $this->get_sizeY());
-    }
-
     public static function new_form($params = null) {
-		$boxes_info = self::get_boxes();
-		if (is_null($params)) {
-			$params['widget_service'] = "localhost.localdomain";
-		}
+	$boxes_info = self::get_boxes();
+	if (is_null($params)) {
+		$params['widget_service'] = gethostname();
+	}
         if (!$params['widget_name'])
             $params['widget_name'] = "CPU";
-        form_generate_input_text("Title", "", "widget_name", null, $params['widget_name'], 5,null);
-		form_generate_input_text("Service", "", "widget_service", null, $params['widget_service'], 20,null);
-		form_generate_select("Box", "", "widget_box", null,  $params['widget_box'], $boxes_info[0], $boxes_info[1]);
+	form_generate_input_text("Title", null, "widget_name", null, $params['widget_name'], 5,null);
+	form_generate_input_text("Service", "The name of the monit 'service' name, usually the hostname", "widget_service", null, $params['widget_service'], 20,null);
+	form_generate_select("Box", null, "widget_box", null,  $params['widget_box'], $boxes_info[0], $boxes_info[1]);
+        form_generate_input_text("Warning threshold", "The percent after which the indicator will display the warning section (orange)", "widget_warning", "n", $params['widget_warning'], 20, "^[0-9]\+(\\\.[0-9]\+)?$");
+        form_generate_input_text("Critical threshold", "The percent after which the indicator will display the warning section (red)", "widget_critical", "n", $params['widget_critical'], 20, "^[0-9]\+(\\\.[0-9]\+)?$");
+	form_generate_input_text("Refresh period", "Period (in seconds) when the widget should updated", "widget_refresh", "y", $params['widget_refresh'], 20, '^([0-9]\+)$');
 	}
 
 	static function get_description() {
